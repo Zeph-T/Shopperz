bb76890714fbb5e9df40b0cda085c05b0110476b
Thari zephaniah <56599367+Zeph-T@users.noreply.github.com>
2021-04-22 19:59:42 +0530

    Added register using strapi

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,6 +9,105 @@ class _LoginScreenState extends State<LoginScreen> {
   var _formKey = GlobalKey<FormState>();
   String _phone, _password;
   bool _obscure = true, _isUser = true;
+
+  Widget _showMobileNumberInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        validator: (val) => val.length != 10 ? 'Phone number invalid' : null,
+        onSaved: (val) => _phone = val,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Phone',
+            icon: Icon(Icons.phone),
+            hintText: 'Enter valid Phone number'),
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        validator: (val) => val.length < 6 ? 'Password too short' : null,
+        obscureText: _obscure,
+        onSaved: (val) => _password = val,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Password',
+            icon: Icon(Icons.lock),
+            suffixIcon: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _obscure = !_obscure;
+                  });
+                },
+                child: _obscure
+                    ? Icon(Icons.visibility)
+                    : Icon(Icons.visibility_off)),
+            hintText: 'Enter Password, min 6 char'),
+      ),
+    );
+  }
+
+  Widget _toggleUserLoginForm() {
+    return RaisedButton(
+      onPressed: () {
+        setState(() {
+          _isUser = true;
+        });
+      },
+      child: Text('User'),
+      color: _isUser ? Colors.grey : Theme.of(context).primaryColor,
+    );
+  }
+
+  Widget _toggleVendorLoginForm() {
+    return RaisedButton(
+      onPressed: () {
+        setState(() {
+          _isUser = false;
+        });
+      },
+      child: Text('Vendor'),
+      color: !_isUser ? Colors.grey : Theme.of(context).primaryColor,
+    );
+  }
+
+  Widget _showSubmitButton() {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          RaisedButton(
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+                print('valid');
+              } else {
+                print('Invalid');
+              }
+            },
+            child: Text(
+              'Submit',
+              style: TextStyle(color: Colors.black),
+            ),
+            elevation: 8,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+            color: Theme.of(context).primaryColor,
+          ),
+          FlatButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/register');
+            },
+            child: Text('New User? Register'),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,106 +130,20 @@ class _LoginScreenState extends State<LoginScreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        RaisedButton(
-                          onPressed: () {
-                            setState(() {
-                              _isUser = true;
-                            });
-                          },
-                          child: Text('User'),
-                          color: _isUser
-                              ? Colors.grey
-                              : Theme.of(context).primaryColor,
-                        ),
+                        _toggleUserLoginForm(),
                         VerticalDivider(
                           color: Colors.white,
                           width: 20,
                         ),
-                        RaisedButton(
-                          onPressed: () {
-                            setState(() {
-                              _isUser = false;
-                            });
-                          },
-                          child: Text('Vendor'),
-                          color: !_isUser
-                              ? Colors.grey
-                              : Theme.of(context).primaryColor,
-                        )
+                        _toggleVendorLoginForm(),
                       ],
                     ),
                     Divider(
                       color: Colors.white,
                     ),
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: TextFormField(
-                        validator: (val) =>
-                            val.length != 10 ? 'Phone number invalid' : null,
-                        onSaved: (val) => _phone = val,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Phone',
-                            icon: Icon(Icons.phone),
-                            hintText: 'Enter valid Phone number'),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: TextFormField(
-                        validator: (val) =>
-                            val.length < 6 ? 'Password too short' : null,
-                        obscureText: _obscure,
-                        onSaved: (val) => _password = val,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                            icon: Icon(Icons.lock),
-                            suffixIcon: GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    _obscure = !_obscure;
-                                  });
-                                },
-                                child: _obscure
-                                    ? Icon(Icons.visibility)
-                                    : Icon(Icons.visibility_off)),
-                            hintText: 'Enter Password, min 6 char'),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                        children: [
-                          RaisedButton(
-                            onPressed: () {
-                              if (_formKey.currentState.validate()) {
-                                _formKey.currentState.save();
-                                print('valid');
-                              } else {
-                                print('Invalid');
-                              }
-                            },
-                            child: Text(
-                              'Submit',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                            elevation: 8,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          FlatButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/register');
-                            },
-                            child: Text('New User? Register'),
-                          )
-                        ],
-                      ),
-                    )
+                    _showMobileNumberInput(),
+                    _showPasswordInput(),
+                    _showSubmitButton()
                   ],
                 ),
               ),
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:core';
 
 class Registerscreen extends StatefulWidget {
   @override
@@ -9,6 +12,147 @@ class _RegisterscreenState extends State<Registerscreen> {
   var _formKey = GlobalKey<FormState>();
   String _phone, _email, _password, _name;
   bool _obscure = true, _isUser = true;
+
+  Widget _showUsernameInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        onSaved: (val) => _name = val,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Name',
+            icon: Icon(Icons.phone),
+            hintText: 'Enter your name'),
+      ),
+    );
+  }
+
+  Widget _showMobileNumberInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        validator: (val) => val.length != 10 ? 'Phone number invalid' : null,
+        onSaved: (val) => _phone = val,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Phone',
+            icon: Icon(Icons.phone),
+            hintText: 'Enter valid Phone number'),
+      ),
+    );
+  }
+
+  Widget _showEmailInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        validator: (val) => !val.contains('@') ? 'Invalid email' : null,
+        onSaved: (val) => _email = val,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Email',
+            icon: Icon(Icons.email),
+            hintText: 'Enter valid email'),
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        validator: (val) => val.length < 6 ? 'Password too short' : null,
+        obscureText: _obscure,
+        onSaved: (val) => _password = val,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Password',
+            icon: Icon(Icons.lock),
+            suffixIcon: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _obscure = !_obscure;
+                  });
+                },
+                child: _obscure
+                    ? Icon(Icons.visibility)
+                    : Icon(Icons.visibility_off)),
+            hintText: 'Enter Password, min 6 char'),
+      ),
+    );
+  }
+
+  Widget _toggleUserForm() {
+    return RaisedButton(
+      onPressed: () {
+        setState(() {
+          _isUser = true;
+        });
+      },
+      child: Text('User'),
+      color: _isUser ? Colors.grey : Theme.of(context).primaryColor,
+    );
+  }
+
+  Widget _toggleVendorForm() {
+    return RaisedButton(
+      onPressed: () {
+        setState(() {
+          _isUser = false;
+        });
+      },
+      child: Text('Vendor'),
+      color: !_isUser ? Colors.grey : Theme.of(context).primaryColor,
+    );
+  }
+
+  Widget _showSubmitButton() {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          RaisedButton(
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+                _registerUser();
+              } else {
+                print('Invalid');
+              }
+            },
+            child: Text(
+              'Submit',
+              style: TextStyle(color: Colors.black),
+            ),
+            elevation: 8,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+            color: Theme.of(context).primaryColor,
+          ),
+          FlatButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/login');
+            },
+            child: Text('Existing User? Login'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _registerUser() async {
+    var response = await http.post(
+        Uri.parse('http://192.168.0.103:1337/auth/local/register'),
+        body: {
+          "username": _name,
+          "email": _email,
+          "password": _password,
+          "mobileNumber": _phone
+        });
+    final responseData = json.decode(response.body);
+    print(responseData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,132 +175,22 @@ class _RegisterscreenState extends State<Registerscreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        RaisedButton(
-                          onPressed: () {
-                            setState(() {
-                              _isUser = true;
-                            });
-                          },
-                          child: Text('User'),
-                          color: _isUser
-                              ? Colors.grey
-                              : Theme.of(context).primaryColor,
-                        ),
+                        _toggleUserForm(),
                         VerticalDivider(
                           color: Colors.white,
                           width: 20,
                         ),
-                        RaisedButton(
-                          onPressed: () {
-                            setState(() {
-                              _isUser = false;
-                            });
-                          },
-                          child: Text('Vendor'),
-                          color: !_isUser
-                              ? Colors.grey
-                              : Theme.of(context).primaryColor,
-                        )
+                        _toggleVendorForm(),
                       ],
                     ),
                     Divider(
                       color: Colors.white,
                     ),
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: TextFormField(
-                        onSaved: (val) => _name = val,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Name',
-                            icon: Icon(Icons.phone),
-                            hintText: 'Enteryour name'),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: TextFormField(
-                        validator: (val) =>
-                            val.length != 10 ? 'Phone number invalid' : null,
-                        onSaved: (val) => _phone = val,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Phone',
-                            icon: Icon(Icons.phone),
-                            hintText: 'Enter valid Phone number'),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: TextFormField(
-                        validator: (val) =>
-                            !val.contains('@') ? 'Invalid email' : null,
-                        onSaved: (val) => _email = val,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Email',
-                            icon: Icon(Icons.email),
-                            hintText: 'Enter valid email'),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: TextFormField(
-                        validator: (val) =>
-                            val.length < 6 ? 'Password too short' : null,
-                        obscureText: _obscure,
-                        onSaved: (val) => _password = val,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                            icon: Icon(Icons.lock),
-                            suffixIcon: GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    _obscure = !_obscure;
-                                  });
-                                },
-                                child: _obscure
-                                    ? Icon(Icons.visibility)
-                                    : Icon(Icons.visibility_off)),
-                            hintText: 'Enter Password, min 6 char'),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                        children: [
-                          RaisedButton(
-                            onPressed: () {
-                              if (_formKey.currentState.validate()) {
-                                _formKey.currentState.save();
-                                print('valid');
-                              } else {
-                                print('Invalid');
-                              }
-                            },
-                            child: Text(
-                              'Submit',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                            elevation: 8,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          FlatButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/login');
-                            },
-                            child: Text('Existing User? Login'),
-                          )
-                        ],
-                      ),
-                    )
+                    _showUsernameInput(),
+                    _showMobileNumberInput(),
+                    _showEmailInput(),
+                    _showPasswordInput(),
+                    _showSubmitButton()
                   ],
                 ),
               ),
