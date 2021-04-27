31c1d3467e62d06ceb7cf2e1d4fae14fa11b8b49
Nithin-Reddy-02 <71481463+Nithin-Reddy-02@users.noreply.github.com>
2021-04-27 18:00:40 +0530

    Add files via upload

---
+++ frontend/lib/components/cart/check_out_card.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '/default_button.dart';
+
+class CheckoutCard extends StatelessWidget {
+  const CheckoutCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: (15),
+        horizontal: (30),
+      ),
+      // height: 174,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -15),
+            blurRadius: 20,
+            color: Color(0xFFDADADA).withOpacity(0.15),
+          )
+        ],
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "Total:\n",
+                    children: [
+                      TextSpan(
+                        text: "\$337.15",
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: (190),
+                  child: DefaultButton(
+                    text: "Place Order",
+                    press: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
